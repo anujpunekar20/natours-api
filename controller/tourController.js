409c843fb42e8a96269f876811ab61d2bd84cd7f
ppman216 <anujkakarot@gmail.com>
2023-02-15 21:58:21 +0530

    files committed

--- controller/tourController.js
+++ controller/tourController.js
@@ -2,28 +2,40 @@
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
+    try{
+        const tours = await Tour.find();
 
-    const tours = await Tour.find();
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tours
+            }
+        });
+    }
+    catch(err){
+        res.status(404).json({
+            status: "fail",
+            message: err
+        })
+    }
+};
 
-    res.status(200).json({
-        status: 'success',
-        timeRequested: req.requestTime,
-        data: {
-            tours
-        }
-});
-}
-
-exports.getToursById = (req, res) => {
-    const id = req.params.id * 1;
-    // const tour = tours.find(x => x.id === id);
-
-        // res.status(200).json({
-        //     status: 'success',
-        //     data: {
-        //         tour
-        //     }
-        // });
+exports.getToursById = async (req, res) => {
+    try{
+        const tour = await Tour.findById(req.params.id);
+        res.status(200).json({
+            status: 'success',
+                data: {
+                    tour
+                }
+            });
+    }catch(err){
+        res.status(404).json({
+            status: "fail",
+            message: err
+        })
+    }
+        
     };
 
 exports.addNewTours = async (req, res) => {
@@ -39,21 +51,42 @@ exports.addNewTours = async (req, res) => {
     } catch(err) {
         res.status(400).json({
             status: 'fail',
-            message: 'Invalid data sent'
+            message: "Invalid data sent"
         });
     }
 }
 
-exports.updateTour = (req,res) => {
-    res.status(201).json({
-        status: 'update',
-        message: 'file(s) updated'
-    });
-}
+exports.updateTour = async (req,res) => {
+    try{
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(201).json({
+            status: 'update',
+            data: {
+                tour
+            }
+        });
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        }) 
+    }
+};
 
-exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: 'deleted',
-        message: null
-    });
+exports.deleteTour = async (req, res) => {
+    try{
+        const tour = await Tour.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status: 'deleted',
+            message: null
+        });
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 }
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,21 +1,68 @@
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
+    // key: {
+    //     type: Number,
+    //     unique: true
+    // },
     name: {
         type: String,
         required: [true, 'Tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true
+    },
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
     },
     price: {
         type: Number,
         required: [true, 'Tour must have a price']
     },
-    rating: {
+    priceDiscount: {
+        type: Number
+    },
+    summary: {
+        type: String,
+        trim: true // trim clears the blank spaces
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
-    }
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0,
+        required: [true, 'A tour must have a summary']
+    },
+    description: {
+        type: String,
+        trim: true 
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
+
+// Tour.count({}, (err, count) => {
+//     console.log(count);
+// })
 
 module.exports = Tour;
