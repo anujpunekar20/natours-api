23e6ca49cbd5d5a94d2328b8123c2bd08738e7ce
Anuj Punekar <anujkakarot@gmail.com>
2023-02-26 20:37:47 +0530

    files modified

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,7 +5,17 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
-router.route('/top-5-tours').get(tourController.aliasTopTours, tourController.getAllTours);
+router  
+    .route('/tour-stats')
+    .get(tourController.getTourStats)
+
+router
+    .route('/monthly-plan')
+    .get(tourController.getMonthlyPlan)
+
+router
+    .route('/top-5-tours')
+    .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
     .route('/')
